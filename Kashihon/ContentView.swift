d35f4555c8c013f652f616ea1f47471b4f6f3087
ame <shachi10969@gmail.com>
2023-07-12 22:12:34 +0900

    Initial Commit

---
+++ Kashihon/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Kashihon
+//
+//  Created by t&a on 2023/07/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kashihon/KashihonApp.swift
@@ -0,0 +1,17 @@
+//
+//  KashihonApp.swift
+//  Kashihon
+//
+//  Created by t&a on 2023/07/12.
+//
+
+import SwiftUI
+
+@main
+struct KashihonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
