68369a74e185bab738c6e91ddc2c0cb198e6b207
ame <shachi10969@gmail.com>
2023-07-15 16:21:27 +0900

    ModelにRealmSwiftを導入&計算プロパティからセキュアなURLを返却

--- Kashihon/Models/Book.swift
+++ Kashihon/Models/Book.swift
@@ -5,16 +5,37 @@
 //  Created by t&a on 2023/07/14.
 //
 
+import RealmSwift
 import UIKit
 
-class Book: Identifiable {
-    var id: String?
-    var title: String?
-    var authors: [String] = []
-    var description: String?
-    var ISBN_13:String?
-    var pageCount: Int?
-    var publishedDate: String?
-    var thumbnailUrl: String?
+class Book: Object, Identifiable {
+    @Persisted var id: String // ID
+    @Persisted var title: String // タイトル
+    @Persisted var authors = RealmSwift.List<String>() // 著書の配列
+    @Persisted var desc: String // 説明
+    @Persisted var ISBN_13: String // ISBN
+    @Persisted var pageCount: Int // ページ数
+    @Persisted var publishedDate: String // 発行日
+    @Persisted var thumbnailUrl: String // サムネイルURL(http)
 
+    // MARK: - ↑　APIから取得する情報
+
+    // MARK: - ↓　個人で蓄積できる情報
+
+    @Persisted var loanName: String // 貸出している人
+    @Persisted var loanDate: Date // 貸出した日付
+    @Persisted var OnLoan: Bool = false // 貸出中フラグ
+    @Persisted var memo: String // 貸出した日付
+
+    /// サムネイルURLをセキュアなURLに変換
+    public var secureThumbnailUrl: URL? {
+        if thumbnailUrl == "" {
+            return nil
+        }
+        let replaceUrlStr = thumbnailUrl.replacingOccurrences(of: "http://", with: "https://")
+        if let url = URL(string: replaceUrlStr) {
+            return url
+        }
+        return nil
+    }
 }
