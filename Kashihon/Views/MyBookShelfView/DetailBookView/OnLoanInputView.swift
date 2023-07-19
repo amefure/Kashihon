fa7133d5281e01807d571a149302b2745cdfadc0
ame <shachi10969@gmail.com>
2023-07-19 09:38:24 +0900

    貸し出し情報登録画面のUI修正

--- Kashihon/Views/MyBookShelfView/DetailBookView/OnLoanInputView.swift
+++ Kashihon/Views/MyBookShelfView/DetailBookView/OnLoanInputView.swift
@@ -20,36 +20,55 @@ struct OnLoanInputView: View {
 
     var body: some View {
         VStack {
-            Text("貸し出した人を入力してください。")
+            HStack {
+                Image(systemName: "figure.wave")
+                Text("貸し出した人")
+                Spacer()
+            }.foregroundColor(.gray)
+
             ZStack(alignment: .leading) {
                 if name.isEmpty {
-                    Text("貸し出した人")
+                    Text("お名前")
                         .fontWeight(.bold)
                         .padding(.leading, 20)
-                        .opacity(0.5)
+                        .opacity(0.6)
                         .zIndex(2)
                         .frame(height: 20)
                 }
                 TextField("", text: $name)
+                    .font(.system(size: 20, weight: .bold))
                     .padding()
                     .background(Color.thema1)
                     .cornerRadius(20).zIndex(1)
             }
 
+            HStack {
+                Image(systemName: "highlighter")
+                Text("貸し出しメモ")
+                Spacer()
+            }.foregroundColor(.gray)
+
             TextField("Memo", text: $memo)
+                .font(.system(size: 20, weight: .bold))
                 .padding()
                 .background(Color.thema2)
                 .cornerRadius(20)
 
-            DatePicker("", selection: $date, displayedComponents: [.date])
+            HStack {
+                Image(systemName: "calendar")
+                Text("貸し出した日")
+                Spacer()
+            }.foregroundColor(.gray)
+            DatePicker("貸し出した日", selection: $date, displayedComponents: [.date])
                 .environment(\.locale, Locale(identifier: "ja_JP"))
                 .environment(\.calendar, Calendar(identifier: .japanese))
-                .pickerStyle(.wheel)
+                .datePickerStyle(.wheel)
                 .colorInvert()
                 .colorMultiply(Color.thema1)
                 .padding(10)
                 .background(Color.thema3)
                 .cornerRadius(20)
+                .labelsHidden()
 
             Button {
                 if validation.validationText(name) {
@@ -59,12 +78,19 @@ struct OnLoanInputView: View {
                 }
             } label: {
                 HStack {
-                    Text("貸し出す")
-                        .fontWeight(.bold)
+                    HStack {
+                        Image(systemName: "hand.wave.fill")
+                        Text("貸し出す")
+                            .fontWeight(.bold)
+                        Image(systemName: "hand.wave.fill")
+                            .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
+                    }
+
                 }.padding()
                     .background(!validation.validationText(name) ? .gray : Color.thema4)
                     .cornerRadius(20)
             }.disabled(!validation.validationText(name))
+                .padding()
         }.padding()
             .foregroundColor(.white)
     }
